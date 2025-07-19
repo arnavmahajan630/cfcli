718a4ca76a854b06c9508f1ba1b7a0415752a3d9
OceanWhisperer <arnavmahajan630@gmail.com>
2025-07-19 19:41:33 +0530

    fix/delete-redundant-comments

--- api/models/internal.models.go
+++ api/models/internal.models.go
@@ -3,20 +3,20 @@ package models
 import "time"
 
 type UserProfile struct {
-	Handle       string // Codeforces handle
-	Rank         string // e.g. "Expert"
-	Rating       int    // current rating
-	MaxRating    int    // max rating achieved
-	Country      string // default to "N/A" if nil
-	Organization string // default to "N/A" if nil
+	Handle       string 
+	Rank         string 
+	Rating       int    
+	MaxRating    int    
+	Country      string 
+	Organization string 
 
-	ProblemCount int    // unique problems solved (from user.status)
-	ContestCount int    // number of rated contests (from user.rating)
-	MaxStreak    string // optional/future; placeholder now
+	ProblemCount int    
+	ContestCount int    
+	MaxStreak    string 
 
-	RecentContest string // name of latest contest
-	RecentRank    int    // rank in latest contest
-	RecentDelta   int    // delta in latest contest (new - old)
+	RecentContest string 
+	RecentRank    int   
+	RecentDelta   int  
 }
 
 type StockResult struct {
